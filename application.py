1d54919bacf88ef66a7a0055a810841e999107d7
Billy McDermott <billy.mcdermott@wk.com>
2013-07-28 15:59:07 -0700

    removed stripe, refactored UI

--- application.py
+++ application.py
@@ -1,44 +1,12 @@
-import os
-from flask import Flask, render_template, request, flash, redirect, url_for
-import stripe
-
-stripe_keys = {
-    'secret_key': os.environ['SECRET_KEY'],
-    'publishable_key': os.environ['PUBLISHABLE_KEY']
-}
-
-stripe.api_key = stripe_keys['secret_key']
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.secret_key = 'ZLO\x8c#5\x9eb\xc2\x88B(\x84R\xc6(e2c\x17"\x8co\xa2'
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', key=stripe_keys['publishable_key'])
-
-
-@app.route('/charge', methods=['POST'])
-def charge():
-    amount = 500
- 
-    customer = stripe.Customer.create(
-        email='',
-        card=request.form['stripeToken']
-    )
-
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=amount,
-        currency='usd',
-        description='Paying Bill'
-    )
-
-    #return render_template('charge.html', amount=amount)
-    flash('Thanks for the payment, you are a champ')
-    return redirect(url_for('index'))
-    return render_template('index.html', amount=amount)
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
